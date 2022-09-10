f886104963780d18ff8bdbdc9bb0b0f551f78758
Ayush Chugh <ayushchugh2006@gmail.com>
2022-09-10 15:26:14 +0530

    fix(style): :bug: tailwind css color class will work now

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,12 @@
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		colors: {
-			primary: "#9e7160",
-			secondary: "rgba(0, 0, 0, 0.85)",
+		extend: {
+			colors: {
+				primary: "#9e7160",
+				secondary: "rgba(0, 0, 0, 0.85)",
+			},
 		},
-		extend: {},
 	},
 	plugins: [],
 };
