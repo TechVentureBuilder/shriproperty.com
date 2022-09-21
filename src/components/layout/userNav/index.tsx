999197b4fade154eb343a613fb56ac70845878cc
Ayush Chugh <ayushchugh2006@gmail.com>
2022-09-21 21:06:44 +0530

    refactor: :recycle: Use custom css instead of ant design in navbar (#167)
    refactor: :recycle: Use custom css instead of ant design in navbar search bar pending
    
    closes #159

--- src/components/layout/userNav/index.tsx
+++ src/components/layout/userNav/index.tsx
@@ -1,13 +1,14 @@
 import { useState, FC, useEffect } from "react";
-import * as Icon from "react-bootstrap-icons";
-import { Drawer, Layout, Menu } from "antd";
+import { Search, Person, List } from "react-bootstrap-icons";
+import { Drawer, Form, Input, Layout, Menu } from "antd";
 import { Link, useLocation } from "react-router-dom";
 import styles from "./index.module.less";
 import { ItemType } from "antd/lib/menu/hooks/useItems";
+import logo from "../../../assets/images/logo/logo.png";
 
 const { Header } = Layout;
 
-const UserNav: FC = () => {
+function UserNav() {
 	const location = useLocation();
 
 	const [open, setOpen] = useState(false);
@@ -24,62 +25,87 @@ const UserNav: FC = () => {
 		setOpen(false);
 	};
 
-	const menuItems: ItemType[] = [
-		{ label: <Link to="/">Home</Link>, key: "/" },
-		{ label: <Link to="/properties">Properties</Link>, key: "/properties" },
-		{ label: <Link to="/contact">Contact US</Link>, key: "/contact" },
-		{ label: <Link to="/list">List Your&#39;s</Link>, key: "/list" },
-	];
-
 	return (
-		<Layout>
-			<Header
-				className="flex flex-row items-center justify-between"
-				style={{ backgroundColor: "white" }}
-			>
-				<div>
-					<img src="/images/logo/logo.png" alt="Logo" className="w-18 h-20 ml-10" />
+		<header>
+			<nav className="flex items-center justify-between px-36 w-full h-28">
+				<div className="max-w-[5rem] h-20">
+					<Link to="/">
+						<img src={logo} alt="logo" className="h-full w-full" />
+					</Link>
 				</div>
 
-				<Menu
-					mode="horizontal"
-					className={`px-5 items-center justify-center hidden md:flex ${styles.menu}`}
-					items={menuItems}
-					selectedKeys={[selectedItem]}
-				/>
-
-				<div className="flex items-center justify-center">
-					<div>
-						<Link to="/" className="text-secondary hover:text-primary h-20">
-							<Icon.PersonCircle size={20} className="mx-5 hover:cursor-pointer " />
+				<ul className="flex list-none items-center justify-between relative mb-0 w-1/3">
+					<li className="text-2xl my-12">
+						<Link
+							to="/"
+							className="text-black relative transition-all after:content-[''] after:transition-all after:duration-300 after:absolute after:bottom-[-3px] after:w-0 after:left-0 after:h-[2px] after:bg-primary hover:text-primary hover:after:w-full"
+						>
+							Home
 						</Link>
-					</div>
-					<div>
-						<Icon.List
-							size={20}
-							className="mr-10 hover:cursor-pointer hover:text-primary"
-							onClick={showDrawer}
+					</li>
+					<li className="text-2xl my-12">
+						<Link
+							to="/properties"
+							className="text-black relative transition-all after:content-[''] after:transition-all after:duration-300 after:absolute after:bottom-[-3px] after:w-0 after:left-0 after:h-[2px] after:bg-primary hover:text-primary hover:after:w-full"
+						>
+							Properties
+						</Link>
+					</li>
+					<li className="text-2xl my-12">
+						<Link
+							to="/contact"
+							className="text-black relative transition-all after:content-[''] after:transition-all after:duration-300 after:absolute after:bottom-[-3px] after:w-0 after:left-0 after:h-[2px] after:bg-primary hover:text-primary hover:after:w-full"
+						>
+							Contact US
+						</Link>
+					</li>
+					<li className="text-2xl my-12">
+						<Link
+							to="/list"
+							className="text-black relative transition-all after:content-[''] after:transition-all after:duration-300 after:absolute after:bottom-[-3px] after:w-0 after:left-0 after:h-[2px] after:bg-primary hover:text-primary hover:after:w-full"
+						>
+							List Your&#39;s
+						</Link>
+					</li>
+
+					{/* <li className="text-2xl my-12 flex items-center group">
+						<input
+							type="text"
+							placeholder="search"
+							className="invisible w-0 p-4 absolute right-0 r-0 bg-primary border-none outline-none rounded text-2xl transition-all duration-500 text-white placeholder:text-white !placeholder:text-4xl group-hover:visible group-hover:opacity-100 group-hover:w-full"
 						/>
-					</div>
-				</div>
-			</Header>
-			<Drawer title="Additional Options" placement="right" onClose={onClose} open={open}>
-				{/* TODO: Convert this to menu */}
+						<Link to="/" className="z-20">
+							<Search className="w-8 h-8 transition-all group-hover:text-white" />
+						</Link>
+					</li> */}
+				</ul>
 
-				<div className="md:hidden block">
-					<p>Home</p>
-					<p>Properties</p>
-					<p>Contact US</p>
-					<p>List Your&#39;s</p>
-				</div>
+				<div className="flex items-center">
+					<Person className="h-10 w-10 mr-5" />
+					<List className="h-10 w-10" />
 
-				{/* NOTE: Sub menu will be here */}
-				<p>Maps</p>
-				<p>Some contents...</p>
-				<p>Some contents...</p>
-			</Drawer>
-		</Layout>
+					<Drawer
+						title="Additional Options"
+						placement="right"
+						onClose={onClose}
+						open={open}
+					>
+						{/* TODO: Convert this to menu */}
+						<div className="md:hidden block">
+							<p>Home</p>
+							<p>Properties</p>
+							<p>Contact US</p>
+							<p>List Your&#39;s</p>
+						</div>
+						{/* NOTE: Sub menu will be here */}
+						<p>Maps</p>
+						<p>Some contents...</p>
+						<p>Some contents...</p>
+					</Drawer>
+				</div>
+			</nav>
+		</header>
 	);
-};
+}
 
 export default UserNav;
